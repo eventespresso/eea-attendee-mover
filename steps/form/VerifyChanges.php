e5cd80f200be6dcfb2865f1461303943669aff89
Brent Christensen <brent@eventespresso.com>
2016-05-04 21:54:45 -0700

    add VerifyChanges class for the Attendee Mover forms; [touch:9589]

---
+++ steps/form/VerifyChanges.php
@@ -0,0 +1,39 @@
+<?php
+namespace AttendeeMover\steps\form;
+
+if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
+	exit( 'No direct script access allowed' );
+}
+
+
+
+/**
+ * Class VerifyChanges
+ * Description
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+class VerifyChanges implements AttendeeMoverStepInterface {
+
+
+
+	public function generate() {
+	}
+
+
+
+	public function display() {
+	}
+
+
+
+	public function process() {
+	}
+
+
+
+}
+// End of file VerifyChanges.php
+// Location: /VerifyChanges.php
