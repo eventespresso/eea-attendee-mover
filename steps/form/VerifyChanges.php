9398f8a4d14b07341aca2353270f9fe5cd138337
Brent Christensen <brent@eventespresso.com>
2016-05-11 15:48:21 -0700

    refactor class to extend SequentialStepForm; set order, form name, admin name, and slug via parent constructor; remove any methods that are either not needed for this form or can use logic from the parent; display some debug info so we can see where everything is happening; [touch:9600]

--- steps/form/VerifyChanges.php
+++ steps/form/VerifyChanges.php
@@ -2,7 +2,7 @@
 namespace AttendeeMover\steps\form;
 
 use EE_Form_Section_Proper;
-use EventEspresso\core\libraries\form_sections\SequentialStepFormInterface;
+use EventEspresso\core\libraries\form_sections\SequentialStepForm;
 
 if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 	exit( 'No direct script access allowed' );
@@ -18,82 +18,21 @@ if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
  * @author        Brent Christensen
  * @since         $VID:$
  */
-class VerifyChanges implements SequentialStepFormInterface {
-
-
+class VerifyChanges extends SequentialStepForm {
 
 	/**
-	 * @return int
-	 */
-	public function order() {
-		return 3;
-	}
-
-
-
-	/**
-	 * a public name for the form that can be displayed on the frontend of a site
+	 * SelectTicket constructor
 	 *
-	 * @return string
+	 * @throws \EventEspresso\Core\Exceptions\InvalidDataTypeException
+	 * @throws \InvalidArgumentException
 	 */
-	public function formName() {
-		return __( 'Verify Changes', 'event_espresso' );
-	}
-
-
-
-	/**
-	 * a public name for the form that can be displayed, but only in the admin
-	 *
-	 * @return string
-	 */
-	public function adminName() {
-		return __( '"Verify Changes" Attendee Mover Step', 'event_espresso' );
-	}
-
-
-
-	/**
-	 * a URL friendly string that can be used for identifying the form
-	 *
-	 * @return string
-	 */
-	public function slug() {
-		return 'verify_changes';
-	}
-
-
-
-	/**
-	 * called after the form is instantiated
-	 * and used for performing any logic that needs to occur early
-	 * before any of the other methods are called.
-	 * returns true if everything is ok to proceed,
-	 * and false if no further form logic should be implemented
-	 *
-	 * @return boolean
-	 */
-	public function initialize() {
-	}
-
-
-
-	/**
-	 * used for localizing any string or variables for use in JS
-	 *
-	 * @return void
-	 */
-	public function localizeVariables() {
-	}
-
-
-
-	/**
-	 * used for setting up css and js
-	 *
-	 * @return void
-	 */
-	public function enqueueStylesAndScripts() {
+	public function __construct() {
+		parent::__construct(
+			3,
+			__( 'Verify Changes', 'event_espresso' ),
+			__( '"Verify Changes" Attendee Mover Step', 'event_espresso' ),
+			'verify_changes'
+		);
 	}
 
 
@@ -104,18 +43,6 @@ class VerifyChanges implements SequentialStepFormInterface {
 	 * @return EE_Form_Section_Proper
 	 */
 	public function generate() {
-		\EEH_Debug_Tools::printr( __FUNCTION__, __CLASS__, __FILE__, __LINE__, 2 );
-	}
-
-
-
-	/**
-	 * takes the generated form and displays it along with ony other non-form HTML that may be required
-	 * returns a string of HTML that can be directly echoed in a template
-	 *
-	 * @return string
-	 */
-	public function display() {
 		\EEH_Debug_Tools::printr( __FUNCTION__, __CLASS__, __FILE__, __LINE__, 2 );
 	}
 
