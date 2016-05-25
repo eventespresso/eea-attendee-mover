72e48ca4f37d9f79aa6a6cb95f9df5d7ba86f3eb
Brent Christensen <brent@eventespresso.com>
2016-05-25 14:54:27 -0700

    fix namespace;[touch:9717]

--- form/Step.php
+++ form/Step.php
@@ -7,7 +7,7 @@ use EE_Ticket;
 use EEM_Event;
 use EEM_Registration;
 use EEM_Ticket;
-use EventEspresso\Core\Exceptions\EntityNotFoundException;
+use EventEspresso\core\exceptions\EntityNotFoundException;
 use EventEspresso\core\libraries\form_sections\SequentialStepForm;
 
 if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
@@ -56,7 +56,7 @@ abstract class Step extends SequentialStepForm {
 	/**
 	 * @param int $REG_ID
 	 * @return \EE_Registration
-	 * @throws \EventEspresso\Core\Exceptions\EntityNotFoundException
+	 * @throws \EventEspresso\core\exceptions\EntityNotFoundException
 	 */
 	protected function getRegistration( $REG_ID = 0 ) {
 		$registration = EEM_Registration::instance()->get_one_by_ID( $REG_ID );
@@ -81,7 +81,7 @@ abstract class Step extends SequentialStepForm {
 	/**
 	 * @param int $EVT_ID
 	 * @return \EE_Event
-	 * @throws \EventEspresso\Core\Exceptions\EntityNotFoundException
+	 * @throws \EventEspresso\core\exceptions\EntityNotFoundException
 	 */
 	protected function getEvent( $EVT_ID = 0 ) {
 		$event = EEM_Event::instance()->get_one_by_ID( $EVT_ID );
@@ -106,7 +106,7 @@ abstract class Step extends SequentialStepForm {
 	/**
 	 * @param int $TKT_ID
 	 * @return \EE_Ticket
-	 * @throws \EventEspresso\Core\Exceptions\EntityNotFoundException
+	 * @throws \EventEspresso\core\exceptions\EntityNotFoundException
 	 */
 	protected function getTicket( $TKT_ID = 0 ) {
 		$ticket = EEM_Ticket::instance()->get_one_by_ID( $TKT_ID );
