bdd58313a2adbfde73f68a450834ae7c1a7229a7
Brent Christensen <brent@eventespresso.com>
2016-06-22 16:53:10 -0700

    add properties for domain services and inject into constructor; convert usage of subcommands to domain services; [touch:9783]

--- services/commands/MoveAttendeeCommandHandler.php
+++ services/commands/MoveAttendeeCommandHandler.php
@@ -1,16 +1,17 @@
 <?php
 namespace EventEspresso\AttendeeMover\services\commands;
 
-use EventEspresso\core\exceptions\EntityNotFoundException;
+use EventEspresso\core\domain\services\registration\CancelRegistrationService;
+use EventEspresso\core\domain\services\registration\CopyRegistrationService;
+use EventEspresso\core\domain\services\registration\CreateRegistrationService;
+use EventEspresso\core\domain\services\registration\UpdateRegistrationService;
+use EventEspresso\core\domain\services\ticket\CreateTicketLineItemService;
 use EventEspresso\core\exceptions\InvalidEntityException;
-use EventEspresso\core\exceptions\InsufficientPermissionsException;
-use EventEspresso\core\services\capabilities\RegistrationsCapChecker;
-use EventEspresso\core\services\commands\CommandBusInterface;
 use EventEspresso\core\services\commands\CommandHandler;
 use EventEspresso\core\services\commands\CommandInterface;
 
-if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
-	exit( 'No direct script access allowed' );
+if ( ! defined('EVENT_ESPRESSO_VERSION')) {
+    exit('No direct script access allowed');
 }
 
 
@@ -28,183 +29,164 @@ if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 class MoveAttendeeCommandHandler extends CommandHandler
 {
 
-	/**
-	 * @var RegistrationsCapChecker $cap_checker
-	 */
-	private $cap_checker;
+
+
+    /**
+     * @var CreateTicketLineItemService $create_ticket_line_item_service
+     */
+    private $create_ticket_line_item_service;
+
+    /**
+     * @var CreateRegistrationService $create_registration_service
+     */
+    private $create_registration_service;
+
+    /**
+     * @var CopyRegistrationService $copy_registration_service
+     */
+    private $copy_registration_service;
+
+    /**
+     * @var CancelRegistrationService $cancel_registration_service
+     */
+    private $cancel_registration_service;
+
+
+    /**
+     * @var UpdateRegistrationService $update_registration_service
+     */
+    private $update_registration_service;
 
 
 
-	/**
-	 * MoveAttendeeCommandHandler constructor.
-	 *
-	 * @param RegistrationsCapChecker $cap_checker
-	 * @param \EE_Registry            $registry
-	 * @param CommandBusInterface     $command_bus
-	 */
-	public function __construct(
-		RegistrationsCapChecker $cap_checker,
-		\EE_Registry $registry,
-		CommandBusInterface $command_bus
-	)
-	{
-		$this->cap_checker = $cap_checker;
-		parent::__construct( $registry, $command_bus );
-	}
+    /**
+     * Command constructor
+     *
+     * @param CreateTicketLineItemService $create_ticket_line_item_service
+     * @param CreateRegistrationService   $create_registration_service
+     * @param CopyRegistrationService     $copy_registration_service
+     * @param CancelRegistrationService   $cancel_registration_service
+     * @param UpdateRegistrationService   $update_registration_service
+     */
+    public function __construct(
+        CreateTicketLineItemService $create_ticket_line_item_service,
+        CreateRegistrationService $create_registration_service,
+        CopyRegistrationService $copy_registration_service,
+        CancelRegistrationService $cancel_registration_service,
+        UpdateRegistrationService $update_registration_service
+    ) {
+        $this->create_ticket_line_item_service = $create_ticket_line_item_service;
+        $this->create_registration_service = $create_registration_service;
+        $this->copy_registration_service = $copy_registration_service;
+        $this->cancel_registration_service = $cancel_registration_service;
+        $this->update_registration_service = $update_registration_service;
+    }
 
 
 
-	/**
-	 * @param CommandInterface $command
-	 * @throws \RuntimeException
-	 * @throws \EventEspresso\core\exceptions\EntityNotFoundException
-	 * @throws \EventEspresso\core\exceptions\UnexpectedEntityException
-	 * @throws \OutOfRangeException
-	 * @return mixed
-	 */
-	public function handle( CommandInterface $command )
-	{
-		/** @var MoveAttendeeCommand $command */
-		if ( ! $command instanceof MoveAttendeeCommand ) {
-			throw new InvalidEntityException( get_class( $command ), 'MoveAttendeeCommand' );
-		}
-		$old_registration = $command->registration();
-		// You can DO IT !!! ... or err... maybe you can't !
-		$this->checkCapabilities( $old_registration );
-		$new_ticket = $command->ticket();
-		// have we already processed this registration change ? if so, then bail...
-		$this->checkIfRegistrationChangeAlreadyProcessed( $old_registration, $new_ticket );
-		// get transaction for original registration
-		$transaction = $this->getTransaction( $old_registration );
-		// create new line item for new ticket
-		$ticket_line_item = $this->executeSubCommand(
-			'EventEspresso\core\services\commands\ticket\CreateTicketLineItemCommand',
-			array( $transaction, $new_ticket, 1 )
-		);
-		// then generate a new registration from that
-		$new_registration = $this->executeSubCommand(
-			'EventEspresso\core\services\commands\registration\CreateRegistrationCommand',
-			array(
-				$transaction,
-				$ticket_line_item,
-				$old_registration->count(),
-				$old_registration->group_size(),
-			)
-		);
-		// move/copy over additional data from old registration, like reg form question answers
-		$this->executeSubCommand(
-			'EventEspresso\core\services\commands\registration\CopyRegistrationDetailsCommand',
-			array( $new_registration, $old_registration )
-		);
-		// and registration payments
-		$this->executeSubCommand(
-			'EventEspresso\core\services\commands\registration\CopyRegistrationPaymentsCommand',
-			array( $new_registration, $old_registration )
-		);
-		// then cancel original line item for ticket
-		$this->executeSubCommand(
-			'EventEspresso\core\services\commands\registration\CancelRegistrationAndTicketLineItemCommand',
-			array( $old_registration )
-		);
-		// perform final status updates and trigger notifications
-		$this->executeSubCommand(
-			'EventEspresso\core\services\commands\registration\UpdateRegistrationAndTransactionAfterChangeCommand',
-			array( $new_registration )
-		);
-		// tag registrations for identification purposes
-		$this->addExtraMeta( $old_registration, $new_registration, $new_ticket );
-		return $new_registration;
-	}
+    /**
+     * @param CommandInterface $command
+     * @throws \RuntimeException
+     * @throws \EventEspresso\core\exceptions\EntityNotFoundException
+     * @throws \EventEspresso\core\exceptions\UnexpectedEntityException
+     * @throws \OutOfRangeException
+     * @return mixed
+     */
+    public function handle(CommandInterface $command)
+    {
+        /** @var MoveAttendeeCommand $command */
+        if ( ! $command instanceof MoveAttendeeCommand) {
+            throw new InvalidEntityException(get_class($command), 'MoveAttendeeCommand');
+        }
+        $old_registration = $command->registration();
+        $new_ticket = $command->ticket();
+        // have we already processed this registration change ? if so, then bail...
+        $this->checkIfRegistrationChangeAlreadyProcessed($old_registration, $new_ticket);
+        // get transaction for original registration
+        $transaction = $old_registration->transaction();
+        // create new line item for new ticket
+        $ticket_line_item = $this->create_ticket_line_item_service->create($transaction, $new_ticket, 1);
+        // then generate a new registration from that
+        $new_registration = $this->create_registration_service->create(
+            $new_ticket->get_related_event(),
+            $transaction,
+            $new_ticket,
+            $ticket_line_item,
+            $old_registration->count(),
+            $old_registration->group_size()
+        );
+        // move/copy over additional data from old registration, like reg form question answers
+        $this->copy_registration_service->copyRegistrationDetails($new_registration, $old_registration);
+        // and registration payments
+        $this->copy_registration_service->copyPaymentDetails($new_registration, $old_registration);
+        // now cancel original registration and it's ticket line item
+        $this->cancel_registration_service->cancelRegistrationAndTicketLineItem($old_registration);
+        // perform final status updates and trigger notifications
+        $this->update_registration_service->updateRegistrationAndTransaction($command->registration());
+        // tag registrations for identification purposes
+        $this->addExtraMeta($old_registration, $new_registration, $new_ticket);
+        return $new_registration;
+    }
 
 
 
-	/**
-	 * @param \EE_Registration $old_registration
-	 * @throws InsufficientPermissionsException
-	 */
-	protected function checkCapabilities( \EE_Registration $old_registration )
-	{
-		$this->cap_checker->editRegistrations(
-			$old_registration,
-			__( 'Edit Registration Ticket Selection', 'event_espresso' )
-		);
-	}
+    /**
+     * @param \EE_Registration $registration
+     * @param \EE_Ticket       $new_ticket
+     * @return bool
+     * @throws \RuntimeException
+     * @throws \EE_Error
+     */
+    protected function checkIfRegistrationChangeAlreadyProcessed(
+        \EE_Registration $registration,
+        \EE_Ticket $new_ticket
+    ) {
+        $reg_moved = $registration->get_extra_meta('registration-moved-to', true, array());
+        if (isset($reg_moved['TKT_ID']) && $reg_moved['TKT_ID'] === $new_ticket->ID()) {
+            $reg_details_url = add_query_arg(
+                array(
+                    'action'  => 'view_registration',
+                    '_REG_ID' => $registration->ID(),
+                ),
+                REG_ADMIN_URL
+            );
+            throw new \RuntimeException(
+                sprintf(
+                    __(
+                        'This exact registration change has already been processed. Please select a different event and/or ticket to change this registration. %3$sThe original cancelled registration can be viewed on the %1$sregistration details admin page%2$s.',
+                        'event_espresso'
+                    ),
+                    '<a href="' . $reg_details_url . '">',
+                    '</a>',
+                    '<br />'
+                )
+            );
+        }
+    }
 
 
 
-	/**
-	 * @param \EE_Registration $registration
-	 * @param \EE_Ticket       $new_ticket
-	 * @return bool
-	 * @throws \RuntimeException
-	 * @throws \EE_Error
-	 */
-	protected function checkIfRegistrationChangeAlreadyProcessed(
-		\EE_Registration $registration,
-		\EE_Ticket $new_ticket
-	) {
-		$reg_moved = $registration->get_extra_meta( 'registration-moved-to', true, array() );
-		if ( isset( $reg_moved['TKT_ID'] ) && $reg_moved['TKT_ID'] === $new_ticket->ID() ) {
-			$reg_details_url = add_query_arg(
-				array(
-					'action'  => 'view_registration',
-					'_REG_ID' => $registration->ID(),
-				),
-				REG_ADMIN_URL
-			);
-			throw new \RuntimeException(
-				sprintf(
-					__(
-						'This exact registration change has already been processed. Please select a different event and/or ticket to change this registration. %3$sThe original cancelled registration can be viewed on the %1$sregistration details admin page%2$s.',
-						'event_espresso'
-					),
-					'<a href="' . $reg_details_url . '">',
-					'</a>',
-					'<br />'
-				)
-			);
-		}
-	}
-
-
-
-	/**
-	 * @param \EE_Registration $registration
-	 * @return \EE_Transaction
-	 * @throws \EventEspresso\core\exceptions\EntityNotFoundException
-	 */
-	protected function getTransaction( \EE_Registration $registration )
-	{
-		$transaction = $registration->transaction();
-		if ( ! $transaction instanceof \EE_Transaction ) {
-			throw new EntityNotFoundException( 'Transaction ID', $registration->transaction_ID() );
-		}
-		return $transaction;
-	}
-
-
-
-	/**
-	 * @param \EE_Registration $old_registration
-	 * @param \EE_Registration $new_registration
-	 * @param \EE_Ticket       $new_ticket
-	 */
-	protected function addExtraMeta(
-		\EE_Registration $old_registration,
-		\EE_Registration $new_registration,
-		\EE_Ticket $new_ticket
-	) {
-		// tag the old registration as moved
-		$old_registration->add_extra_meta(
-			'registration-moved-to',
-			array( 'TKT_ID' => $new_ticket->ID(), 'NEW_REG_ID' => $new_registration->ID() )
-		);
-		// and the new registration as well
-		$new_registration->add_extra_meta(
-			'registration-moved-from',
-			array( 'TKT_ID' => $new_ticket->ID(), 'OLD_REG_ID' => $old_registration->ID() )
-		);
-	}
+    /**
+     * @param \EE_Registration $old_registration
+     * @param \EE_Registration $new_registration
+     * @param \EE_Ticket       $new_ticket
+     */
+    protected function addExtraMeta(
+        \EE_Registration $old_registration,
+        \EE_Registration $new_registration,
+        \EE_Ticket $new_ticket
+    ) {
+        // tag the old registration as moved
+        $old_registration->add_extra_meta(
+            'registration-moved-to',
+            array('TKT_ID' => $new_ticket->ID(), 'NEW_REG_ID' => $new_registration->ID())
+        );
+        // and the new registration as well
+        $new_registration->add_extra_meta(
+            'registration-moved-from',
+            array('TKT_ID' => $new_ticket->ID(), 'OLD_REG_ID' => $old_registration->ID())
+        );
+    }
 
 }
 // End of file MoveAttendeeCommandHandler.php
