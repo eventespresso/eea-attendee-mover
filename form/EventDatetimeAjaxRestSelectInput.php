99dc3fb07ae8818984fdce5c7337de6ebaf3404b
Brent Christensen <brent@eventespresso.com>
2016-08-12 13:36:27 -0700

    use esc_html__(); [touch:9950]

---
+++ form/EventDatetimeAjaxRestSelectInput.php
@@ -0,0 +1,22 @@
+<?php
+namespace EventEspresso\AttendeeMover\form;
+
+defined( 'ABSPATH' ) || exit;
+
+
+
+/**
+ * Class EventDatetimeAjaxRestSelectInput
+ * Description
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+class EventDatetimeAjaxRestSelectInput {
+
+
+
+}
+// End of file EventDatetimeAjaxRestSelectInput.php
+// Location: EventEspresso\AttendeeMover\form/EventDatetimeAjaxRestSelectInput.php
