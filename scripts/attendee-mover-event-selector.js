b5c0721ff1b796222a44f45dcda63d138b630a08
Brent Christensen <brent@eventespresso.com>
2016-08-15 14:42:07 -0700

    add new JS file for processing API requests; [touch:9950]

---
+++ scripts/attendee-mover-event-selector.js
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function EE_Attendee_Mover_Event_Select2( data_interface_args ) {
+	this.default_query_params = data_interface_args.default_query_params || {};
+	this.items_per_page = this.default_query_params.limit || 10;
+	this.nonce = data_interface_args.nonce;
+
+	/**
+	 * Changes the request params set by select2 and prepares them for an EE4 REST request
+	 * @param {object} params
+	 * @returns object
+	 */
+	this.prepData = function ( params ) {
+		params.page = params.page || 1;
+		var new_params =  this.default_query_params;
+		new_params.limit = [
+			( params.page - 1 ) * this.items_per_page,
+			this.items_per_page
+		];
+		if( typeof new_params.where === 'undefined' ) {
+			new_params.where = {};
+		}
+		var search_term = params.term || '';
+		new_params.where.EVT_name= [ 'like', '%' + search_term + '%' ];
+		new_params.include='EVT_ID, EVT_name, Datetime.DTT_name, Datetime.DTT_EVT_start, Datetime.DTT_EVT_end, Datetime.DTT_is_primary';
+		new_params._wpnonce = this.nonce;
+		return new_params;
+	};
+
+	/**
+	 * Sets the wp nonce header for authentication
+	 * @param {object} xhr
+	 * @returns void
+	 */
+	this.beforeSend = function( xhr ) {
+		 xhr.setRequestHeader( 'X-WP-Nonce', this.nonce );
+	};
+
+	/**
+	 * Takes incoming EE4 REST API response and turns into a data format select2 can handle
+	 * @param {object} data
+	 * @param {object} params
+	 * @returns object
+	 */
+	this.processResults = function ( data, params ){
+		// console_log( 'processResults', '', true );
+		// console_log_object( 'data', data, 0 );
+		// console_log_object( 'params', params, 0 );
+		var formatted_results = [];
+		for( var i=0; i<data.length; i++ ) {
+			//find the primary datetime's name
+			var preferred_datetime_text = '';
+			var secs       = 0;
+			var start_date = null;
+			var start_date_string = '';
+			var end_date = null;
+			var end_date_string = '';
+
+			for( var j=0; j<data[i].datetimes.length; j++) {
+				if( data[i].datetimes[j].DTT_is_primary || preferred_datetime_text == '' ) {
+					if( data[i].datetimes[j].DTT_name !== '' ) {
+						preferred_datetime_text = data[ i ].datetimes[ j ].DTT_name + ': ';
+					}
+					// console_log_object( 'DTT_EVT_start', data[ i ].datetimes[ j ].DTT_EVT_start, 0 );
+					// console_log_object( 'DTT_EVT_end', data[ i ].datetimes[ j ].DTT_EVT_end, 0 );
+					secs = Date.parse( data[i].datetimes[j].DTT_EVT_start );
+					// start date
+					start_date = new Date( secs );
+					start_date_string = start_date.toLocaleString("en-us", { month: "short" }) + ' ' + start_date.getDate();
+					preferred_datetime_text += start_date_string;
+					// end date
+					secs     = Date.parse( data[ i ].datetimes[ j ].DTT_EVT_end );
+					end_date = new Date( secs );
+					end_date_string = end_date.toLocaleString( "en-us", { month : "short" } ) + ' ' + end_date.getDate();
+					if ( end_date_string !== start_date_string ) {
+						preferred_datetime_text += ' - ' + end_date_string;
+					}
+
+				}
+			}
+			// console_log_object( 'preferred_datetime_text', preferred_datetime_text, 0 );
+			formatted_results.push(
+				{
+					id: data[i]['EVT_ID'],
+					text: data[i]['EVT_name'] + ' (' + preferred_datetime_text + ')'
+				}
+			);
+		}
+		params.page = params.page || 1;
+
+		return {
+		  results: formatted_results,
+		  pagination: {
+			more: data.length == this.items_per_page
+		  }
+		}
+	};
+}
+
+// scripts/attendee-mover-event-selector.js:86
