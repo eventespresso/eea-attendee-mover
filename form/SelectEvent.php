e7eed03b5cf84f5bfe97ec379703bc57aee2bc55
Brent Christensen <brent@eventespresso.com>
2016-08-17 10:13:33 -0700

    remove endpoint param; add limit query param; [touch:9950]

--- form/SelectEvent.php
+++ form/SelectEvent.php
@@ -72,7 +72,6 @@ class SelectEvent extends Step {
 								'html_label_text'    => esc_html__( 'Select New Event', 'event_espresso' ),
 								'model_name'         => 'Event',
 								'display_field_name' => 'EVT_name',
-								'endpoint'           => 'events?include=EVT_name,Datetime.DTT_ID,Datetime.DTT_is_primary,Datetime.DTT_EVT_start,Datetime.DTT_EVT_end',
 								'query_params'       => array(
 									0 => apply_filters(
                                         'FHEE__AttendeeMover_form_SelectEvent__generate__where_parameters',
@@ -83,6 +82,7 @@ class SelectEvent extends Step {
                                             )
                                         )
                                     ),
+									'limit' => 10,
 									'caps'  => \EEM_Base::caps_read_admin
 								),
 								'required'           => true,
