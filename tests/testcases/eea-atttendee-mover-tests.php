c274954e801407254da2de768bde803448cb3dab
Darren Ethier <darren@roughsmootheng.in>
2017-04-20 13:52:19 -0400

    update attendee mover tests bootstrap and config [touch: 10623]

---
+++ tests/testcases/eea-atttendee-mover-tests.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Test class for espresso_addon_skeleton.php
+ *
+ * @since         1.0.0
+ * @package       EventEspresso/AttendeeMover
+ * @subpackage    tests
+ */
+class eea_attendee_mover_tests extends EE_UnitTestCase
+{
+
+    /**
+     * Tests the loading of the main file
+     */
+    function test_loading_new_addon()
+    {
+        $this->assertEquals(has_action('AHEE__EE_System__load_espresso_addons', 'load_espresso_new_addon'), 10);
+        $this->assertTrue(class_exists('EE_Attendee_Mover'));
+    }
+}
