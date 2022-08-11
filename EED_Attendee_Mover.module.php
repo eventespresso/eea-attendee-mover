eac618a731f6068554f2ce661b6f2f197b7e1657
Brent Christensen <brent@eventespresso.com>
2022-08-11 11:16:40 -0700

    add admin_page param to edit_attendee_selections()

--- EED_Attendee_Mover.module.php
+++ EED_Attendee_Mover.module.php
@@ -15,23 +15,21 @@ use EventEspresso\core\services\loaders\LoaderFactory;
  */
 class EED_Attendee_Mover extends EED_Module
 {
-
-
     /**
      * @var Registrations_Admin_Page $admin_page
      */
     protected static $admin_page;
 
 
-
     /**
      * @return EED_Attendee_Mover|EED_Module
+     * @throws EE_Error
+     * @throws ReflectionException
      */
     public static function instance()
     {
         return parent::get_instance(__CLASS__);
     }
-
 
 
     /**
@@ -44,7 +42,6 @@ class EED_Attendee_Mover extends EED_Module
     }
 
 
-
     /**
      * set_hooks_admin - for hooking into EE Admin Core, other modules, etc
      *
@@ -54,58 +51,56 @@ class EED_Attendee_Mover extends EED_Module
     {
         add_action(
             'FHEE__EE_Admin_Page___load_page_dependencies__after_load__espresso_registrations__edit_attendee_selections',
-            array('EED_Attendee_Mover', 'edit_attendee_selections_init')
+            ['EED_Attendee_Mover', 'edit_attendee_selections_init']
         );
         add_filter(
             'FHEE__EE_Registrations_List_Table__column_actions__actions',
-            array('EED_Attendee_Mover', 'edit_attendee_selections_button_reg_list'),
+            ['EED_Attendee_Mover', 'edit_attendee_selections_button_reg_list'],
             10,
             2
         );
         add_action(
             'AHEE__reg_admin_details_main_meta_box_reg_details__top',
-            array('EED_Attendee_Mover', 'edit_attendee_selections_button'),
+            ['EED_Attendee_Mover', 'edit_attendee_selections_button'],
             10,
             1
         );
         add_action(
             'AHEE__reg_status_change_buttons__after_header',
-            array('EED_Attendee_Mover', 'registration_moved_notice'),
+            ['EED_Attendee_Mover', 'registration_moved_notice'],
             10,
             1
         );
         add_filter(
             'FHEE__Extend_Registrations_Admin_Page__page_setup__page_routes',
-            array('EED_Attendee_Mover', 'attendee_mover_page_routes'),
+            ['EED_Attendee_Mover', 'attendee_mover_page_routes'],
             10,
             2
         );
         add_filter(
             'FHEE__Extend_Registrations_Admin_Page__page_setup__page_config',
-            array('EED_Attendee_Mover', 'attendee_mover_page_config'),
+            ['EED_Attendee_Mover', 'attendee_mover_page_config'],
             10,
             2
         );
         add_filter(
             'FHEE__EE_Admin_Page___display_legend__items',
-            array('EED_Attendee_Mover', 'reg_admin_list_legend'),
+            ['EED_Attendee_Mover', 'reg_admin_list_legend'],
             10,
             1
         );
     }
 
 
-
     /**
      * run - initial module setup
      *
-     * @param  WP $WP
+     * @param WP $WP
      * @return    void
      */
     public function run($WP)
     {
     }
-
 
 
     /**
@@ -117,38 +112,37 @@ class EED_Attendee_Mover extends EED_Module
      */
     public static function attendee_mover_page_routes(array $page_routes, Registrations_Admin_Page $admin_page)
     {
-        EED_Attendee_Mover::$admin_page = $admin_page;
-        $req_data = $admin_page->get_request_data();
-        $REG_ID = ! empty($req_data['_REG_ID']) && ! is_array($req_data['_REG_ID'])
+        EED_Attendee_Mover::$admin_page             = $admin_page;
+        $req_data                                   = $admin_page->get_request_data();
+        $REG_ID                                     = ! empty($req_data['_REG_ID']) && ! is_array($req_data['_REG_ID'])
             ? $req_data['_REG_ID']
             : 0;
-        $EVT_ID = ! empty($req_data['EVT_ID']) && ! is_array($req_data['EVT_ID'])
+        $EVT_ID                                     = ! empty($req_data['EVT_ID']) && ! is_array($req_data['EVT_ID'])
             ? $req_data['EVT_ID']
             : 0;
-        $TKT_ID = ! empty($req_data['TKT_ID']) && ! is_array($req_data['TKT_ID'])
+        $TKT_ID                                     = ! empty($req_data['TKT_ID']) && ! is_array($req_data['TKT_ID'])
             ? $req_data['TKT_ID']
             : 0;
-        $page_routes['edit_attendee_selections'] = array(
-            'func'       => array('EED_Attendee_Mover', 'edit_attendee_selections'),
+        $page_routes['edit_attendee_selections']    = [
+            'func'       => ['EED_Attendee_Mover', 'edit_attendee_selections'],
             'capability' => 'ee_edit_registration',
             'obj_id'     => $REG_ID,
             '_REG_ID'    => $REG_ID,
             'EVT_ID'     => $EVT_ID,
             'TKT_ID'     => $TKT_ID,
-        );
-        $page_routes['process_attendee_selections'] = array(
-            'func'       => array('EED_Attendee_Mover', 'process_attendee_selections'),
-            'args'       => array($admin_page),
+        ];
+        $page_routes['process_attendee_selections'] = [
+            'func'       => ['EED_Attendee_Mover', 'process_attendee_selections'],
+            'args'       => [$admin_page],
             'capability' => 'ee_edit_registration',
             'obj_id'     => $REG_ID,
             '_REG_ID'    => $REG_ID,
             'EVT_ID'     => $EVT_ID,
             'TKT_ID'     => $TKT_ID,
             'noheader'   => true,
-        );
+        ];
         return $page_routes;
     }
-
 
 
     /**
@@ -156,37 +150,36 @@ class EED_Attendee_Mover extends EED_Module
      *
      * @param array                    $page_config current page config.
      * @param Registrations_Admin_Page $admin_page
-     * @since  1.0.0
      * @return array
+     * @since  1.0.0
      */
     public static function attendee_mover_page_config($page_config, Registrations_Admin_Page $admin_page)
     {
-        EED_Attendee_Mover::$admin_page = $admin_page;
-        $req_data = $admin_page->get_request_data();
-        $page_config['edit_attendee_selections'] = array(
-            'nav'           => array(
+        EED_Attendee_Mover::$admin_page          = $admin_page;
+        $req_data                                = $admin_page->get_request_data();
+        $page_config['edit_attendee_selections'] = [
+            'nav'           => [
                 'label'      => esc_html__('Change Event/Ticket Selection', 'event_espresso'),
                 'order'      => 15,
                 'persistent' => false,
                 'url'        => isset($req_data['_REG_ID'])
                     ? EE_Admin_Page::add_query_args_and_nonce(
-                        array(
+                        [
                             'action'  => 'edit_attendee_selections',
                             '_REG_ID' => $req_data['_REG_ID'],
-                        ),
+                        ],
                         $admin_page->get_current_page_view_url()
                     )
                     : $admin_page->admin_base_url(),
-            ),
+            ],
             'metaboxes'     => array_merge(
                 $admin_page->default_espresso_metaboxes(),
-                array(array('EED_Attendee_Mover', 'add_edit_attendee_selections_meta_box'))
+                [['EED_Attendee_Mover', 'add_edit_attendee_selections_meta_box']]
             ),
             'require_nonce' => true,
-        );
+        ];
         return $page_config;
     }
-
 
 
     /**
@@ -197,7 +190,7 @@ class EED_Attendee_Mover extends EED_Module
         add_meta_box(
             'edit-attendee-selection-mbox',
             esc_html__('Change Event/Ticket Selection', 'event_espresso'),
-            array('EED_Attendee_Mover', 'edit_attendee_selections_meta_box'),
+            ['EED_Attendee_Mover', 'edit_attendee_selections_meta_box'],
             EED_Attendee_Mover::$admin_page->wp_page_slug(),
             'normal',
             'high'
@@ -215,18 +208,17 @@ class EED_Attendee_Mover extends EED_Module
      * @throws InvalidArgumentException
      * @throws EE_Error
      */
-    public static function edit_attendee_selections_button_reg_list(
-        array $actions,
-        EE_Registration $registration
-    ) {
-        if (! in_array(
-            $registration->status_ID(),
-            array(
-                EEM_Registration::status_id_cancelled,
-                EEM_Registration::status_id_declined,
-            ),
-            true
-        )
+    public static function edit_attendee_selections_button_reg_list(array $actions, EE_Registration $registration)
+    {
+        if (
+            ! in_array(
+                $registration->status_ID(),
+                [
+                    EEM_Registration::status_id_cancelled,
+                    EEM_Registration::status_id_declined,
+                ],
+                true
+            )
         ) {
             $actions['edit_attendee_selections'] = '<li>' .
                                                    EED_Attendee_Mover::edit_attendee_selections_button(
@@ -251,7 +243,8 @@ class EED_Attendee_Mover extends EED_Module
      */
     public static function edit_attendee_selections_button($REG_ID = 0, $button = true, $echo = true)
     {
-        if ($REG_ID === 0
+        if (
+            $REG_ID === 0
             || ! EE_Registry::instance()->CAP->current_user_can(
                 'ee_edit_registration',
                 'espresso_registrations_change_event_or_ticket',
@@ -262,10 +255,10 @@ class EED_Attendee_Mover extends EED_Module
         }
         $url = EED_Attendee_Mover::get_edit_attendee_selections_url($REG_ID);
         if ($button) {
-            $link_text = $link_label = esc_html__(' Change Event/Ticket Selection', 'event_espresso');
+            $link_text  = $link_label = esc_html__(' Change Event/Ticket Selection', 'event_espresso');
             $link_class = 'button secondary-button right';
         } else {
-            $link_text = '';
+            $link_text  = '';
             $link_label = esc_html__(' Change Event/Ticket Selection', 'event_espresso');
             $link_class = 'right';
         }
@@ -284,7 +277,6 @@ class EED_Attendee_Mover extends EED_Module
     }
 
 
-
     /**
      * @param array $items
      * @return array
@@ -296,18 +288,17 @@ class EED_Attendee_Mover extends EED_Module
             // insert attendee_mover icon before the "approved_status" icon
             $items = EEH_Array::insert_into_array(
                 $items,
-                array(
-                    'attendee_mover' => array(
+                [
+                    'attendee_mover' => [
                         'class' => 'dashicons dashicons-controls-repeat',
                         'desc'  => esc_html__('Change Event/Ticket Selection', 'event_espresso'),
-                    ),
-                ),
+                    ],
+                ],
                 'approved_status'
             );
         }
         return $items;
     }
-
 
 
     /**
@@ -325,12 +316,12 @@ class EED_Attendee_Mover extends EED_Module
             );
         }
         return EE_Admin_Page::add_query_args_and_nonce(
-            array(
+            [
                 'action'  => $process
                     ? 'process_attendee_selections'
                     : 'edit_attendee_selections',
                 '_REG_ID' => $REG_ID,
-            ),
+            ],
             REG_ADMIN_URL
         );
     }
@@ -347,31 +338,31 @@ class EED_Attendee_Mover extends EED_Module
     public static function registration_moved_notice($REG_ID)
     {
         /** @var EE_Registration $registration */
-        $registration = EEM_Registration::instance()->get_one_by_ID($REG_ID);
-        $reg_moved_meta = array(
-            'registration-moved-to'   => array(
+        $registration   = EEM_Registration::instance()->get_one_by_ID($REG_ID);
+        $reg_moved_meta = [
+            'registration-moved-to'   => [
                 'meta_key' => 'NEW_REG_ID',
                 'message'  => esc_html__(
                     '%1$sThis registration was cancelled and moved to a %2$snew registration%3$s.%4$s',
                     'event_espresso'
                 ),
-            ),
-            'registration-moved-from' => array(
+            ],
+            'registration-moved-from' => [
                 'meta_key' => 'OLD_REG_ID',
                 'message'  => esc_html__(
                     '%1$sThis registration was moved from a %2$sprevious registration%3$s which has been cancelled.%4$s',
                     'event_espresso'
                 ),
-            ),
-        );
+            ],
+        ];
         foreach ($reg_moved_meta as $to_or_from => $reg_meta) {
-            $reg_moved = $registration->get_extra_meta($to_or_from, true, array());
+            $reg_moved = $registration->get_extra_meta($to_or_from, true, []);
             if (isset($reg_meta['meta_key'], $reg_moved[ $reg_meta['meta_key'] ], $reg_meta['message'])) {
                 $reg_details_url = add_query_arg(
-                    array(
+                    [
                         'action'  => 'view_registration',
                         '_REG_ID' => $reg_moved[ $reg_meta['meta_key'] ],
-                    ),
+                    ],
                     REG_ADMIN_URL
                 );
                 echo sprintf(
@@ -399,17 +390,16 @@ class EED_Attendee_Mover extends EED_Module
         if (! $form_steps_manager instanceof StepsManager) {
             /** @var EventEspresso\core\services\loaders\Loader $loader */
             $loader             = LoaderFactory::getLoader();
-            /** @var EventEspresso\core\services\request\RequestInterface $request */
-            $request            = $loader->getShared('EventEspresso\core\services\request\RequestInterface');
-            $REG_ID             = $request->getRequestParam('_REG_ID', 0, 'int');
+            $request            = $loader->getShared('EE_Request');
+            $REG_ID             = absint($request->get('_REG_ID', 0));
             $form_steps_manager = $loader->getShared(
                 'EventEspresso\AttendeeMover\form\StepsManager',
-                array(
+                [
                     // base redirect URL
                     EED_Attendee_Mover::get_edit_attendee_selections_url($REG_ID, $process),
                     // default step slug
                     'select_event',
-                )
+                ]
             );
         }
         return $form_steps_manager;
@@ -439,22 +429,22 @@ class EED_Attendee_Mover extends EED_Module
         } catch (Exception $e) {
             new ExceptionStackTraceDisplay($e);
         }
-        add_action('admin_enqueue_scripts', array($this, 'enqueue_scripts'), 1);
+        add_action('admin_enqueue_scripts', [$this, 'enqueue_scripts'], 1);
     }
 
 
     /**
      * callback that displays the page template
      *
+     * @param EE_Admin_Page $admin_page
      * @throws EE_Error
      * @throws DomainException
      */
-    public static function edit_attendee_selections()
+    public static function edit_attendee_selections(EE_Admin_Page $admin_page)
     {
         // the details template wrapper
-        EED_Attendee_Mover::$admin_page->display_admin_page_with_sidebar();
+        $admin_page->display_admin_page_with_sidebar();
     }
-
 
 
     /**
@@ -477,12 +467,12 @@ class EED_Attendee_Mover extends EED_Module
         try {
             $form_steps_manager = $this->get_form_steps_manager();
             echo $form_steps_manager->displayProgressSteps();
+            // echo \EEH_HTML::h1( $form_steps_manager->getCurrentStep()->formName() );
             echo $form_steps_manager->displayCurrentStepForm();
         } catch (Exception $e) {
             new ExceptionStackTraceDisplay($e);
         }
     }
-
 
 
     /**
@@ -500,7 +490,7 @@ class EED_Attendee_Mover extends EED_Module
     /**
      * callback route for when the attendee mover step forms are being processed
      *
-     * @param  Registrations_Admin_Page $admin_page
+     * @param Registrations_Admin_Page $admin_page
      * @throws Exception
      */
     protected function _process_attendee_selections(Registrations_Admin_Page $admin_page)
@@ -512,7 +502,6 @@ class EED_Attendee_Mover extends EED_Module
             new ExceptionStackTraceDisplay($e);
         }
     }
-
 
 
     /**
